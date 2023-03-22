5a18c5c76d8b463d5a1604be2fd5d258d5b6ac19
RiyaSharma82308 <riya82308@gmail.com>
2023-03-22 22:55:38 +0530

    LongestRepeatingSubsequence

---
+++ 8_LongestRepeatingSubsequence.cpp
@@ -0,0 +1,45 @@
+//{ Driver Code Starts
+#include<bits/stdc++.h>
+using namespace std;
+
+// } Driver Code Ends
+class Solution {
+	public:
+		int LongestRepeatingSubsequence(string str){
+		    // Code here
+		    vector<vector<int>>dp(str.length()+1,vector<int>(str.length()+1));
+		    for(int i=str.length();i>=0;i--)
+		    {
+		        for(int j=str.length();j>=0;j--)
+		        {
+		            if(i==str.length() || j==str.length()) dp[i][j]=0;
+		            else{
+		                if(str[i]==str[j] && i!=j)
+		                {
+		                    dp[i][j]=dp[i+1][j+1]+1;
+		                }
+		                else{
+		                    dp[i][j]=max(dp[i+1][j],dp[i][j+1]);
+		                }
+		            }
+		        }
+		    }
+		    return dp[0][0];
+		}
+
+};
+
+//{ Driver Code Starts.
+int main(){
+	int tc;
+	cin >> tc;
+	while(tc--){
+		string str;
+		cin >> str;
+		Solution obj;
+		int ans = obj.LongestRepeatingSubsequence(str);
+		cout << ans << "\n";
+	}
+	return 0;
+}
+// } Driver Code Ends
