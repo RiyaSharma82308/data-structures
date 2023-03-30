5299a0cd3987a3924ea4e5a7251e11180d12b119
RiyaSharma82308 <riya82308@gmail.com>
2023-03-30 19:21:51 +0530

    min swaps for bracket balancing

---
+++ strings/29_minswapsforbracketbalance.cpp
@@ -0,0 +1,40 @@
+//{ Driver Code Starts
+#include<bits/stdc++.h> 
+using namespace std; 
+
+// } Driver Code Ends
+class Solution{   
+public:
+    int minimumNumberOfSwaps(string S){
+        // code here 
+        int ans=0;
+        int count=0;
+        for(int i=0;i<S.length();i++)
+        {
+            if(S[i]=='['){
+                count++;
+            }
+            else{
+                count--;
+                if(count<0) ans-=count;
+            }
+        }
+        return ans;
+    }
+};
+
+//{ Driver Code Starts.
+int main() 
+{ 
+    int t;
+    cin>>t;
+    while(t--)
+    {
+        string S;
+        cin >> S;
+        Solution ob;
+        cout << ob.minimumNumberOfSwaps(S) << endl;
+    }
+    return 0; 
+} 
+// } Driver Code Ends
