40a8452b40bbdc20bbb574bc1d21f60310725f35
RiyaSharma82308 <riya82308@gmail.com>
2023-04-06 18:41:13 +0530

    Number of customers who could not get a computer

---
+++ strings/36_cunstomersgetcomp.cpp
@@ -0,0 +1,39 @@
+#include<bits/stdc++.h>
+#include<cstring>
+using namespace std;
+ 
+#define MAX_CHAR 26
+int  runCustomerSimulation(int n, string str)
+{
+    vector<int>vec(MAX_CHAR,0);
+    int count=0;
+    int ans=0;
+    for(int i=0;i<str.length();i++)
+    {
+        vec[str[i]-'A']++;
+        if(vec[str[i]-'A']==1) {
+            count++;
+            // cout<<vec[str[i]-'A']<<' '<<str[i]<<' '<<count<<endl;
+            if(count>n) {
+                ans++;
+                // cout<<"answer increased "<<ans<<endl;
+            }
+        }
+        if(vec[str[i]-'A']==2) {
+            count--;
+            // cout<<vec[str[i]-'A']<<' '<<str[i]<<' '<<"here "<<count<<endl;
+        }
+
+    }
+    return ans;
+}
+// Driver program
+int main()
+{
+    cout << runCustomerSimulation(2, "ABBAJJKZKZ") << endl;
+    cout << runCustomerSimulation(3, "GACCBDDBAGEE") << endl;
+    cout << runCustomerSimulation(3, "GACCBGDDBAEE") << endl;
+    cout << runCustomerSimulation(1, "ABCBCA") << endl;
+    cout << runCustomerSimulation(1, "ABCBCADEED") << endl;
+    return 0;
+}
